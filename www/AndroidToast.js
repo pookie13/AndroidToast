76c2913d84897f4100ec22b868ec1af630781268
pookie piyush <piyush.gupta202390@gmail.com>
2016-08-11 17:06:47 +0530

    Add setAudioMode() bridge in AndroidToast.js

--- www/AndroidToast.js
+++ www/AndroidToast.js
@@ -1,5 +1,10 @@
 var exec = require('cordova/exec');
 
+exports.SPEAKER = 'speaker';
+exports.EARPIECE = 'earpiece';
+exports.NORMAL = 'normal';
+exports.RINGTONE = 'ringtone';
+
 exports.toast = function(arg0) {
     exec(null, null, "AndroidToast", "toast", [arg0]);
 };
@@ -7,4 +12,9 @@ exports.toast = function(arg0) {
 
 exports.play = function(arg0) {
     exec(null, null, "AndroidToast", "play", [arg0]);
+};
+
+
+exports.setAudioMode = function (mode) {
+	cordova.exec(null, null, 'AndroidToast', 'setAudioMode', [mode]);
 };
