25507cca903163bc7de31df5a944600de8149ba1
pookie piyush <piyush.gupta202390@gmail.com>
2016-08-17 12:45:19 +0530

    Update AndroidToast.js
    added methods for checking permissions

--- www/AndroidToast.js
+++ www/AndroidToast.js
@@ -5,6 +5,11 @@ exports.EARPIECE = 'earpiece';
 exports.NORMAL = 'normal';
 exports.RINGTONE = 'ringtone';
 
+exports.SMS = 'Manifest.permission.READ_SMS';
+exports.AUDIO_TOGEL = 'Manifest.permission.MODIFY_AUDIO_SETTING';
+
+
+
 exports.toast = function(arg0) {
     exec(null, null, "AndroidToast", "toast", [arg0]);
 };
@@ -17,4 +22,11 @@ exports.play = function(arg0) {
 
 exports.setAudioMode = function (mode) {
 	cordova.exec(null, null, 'AndroidToast', 'setAudioMode', [mode]);
+};
+
+exports.checkForSMSPermission = function () {
+	cordova.exec(null, null, 'AndroidToast', 'checkForSMSPermission', []);
+};
+exports.checkForRecordPermission = function () {
+	cordova.exec(null, null, 'AndroidToast', 'checkForRecordPermission', []);
 };
